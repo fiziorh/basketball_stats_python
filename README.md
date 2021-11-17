36eac9973a6653bf74cf57bd7f5524753e67ec8a
fiziorh <fizio69@gmail.com>
2021-11-17 19:12:19 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# basketball_stats_python
+ 
---
+++ app.py
@@ -0,0 +1,21 @@
+import constants
+import copy
+
+if __name__ == "__main__":
+    players = copy.deepcopy(constants.PLAYERS)
+    teams = copy.deepcopy(constants.TEAMS)
+
+    def clean_data():
+        for player in players:
+            # first split the string into 2 combined string
+            player['height'] = player['height'].split()
+            # then pick the first string and cast it into integers
+            player['height'] = int(player['height'][0])
+
+            # if the experience key contains "YES" string, then change it into True boolean value
+            if player['experience'] == 'YES':
+                player['experience'] = True
+            else:
+                player['experience'] = False
+
+        return players
